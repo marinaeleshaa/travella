9e51aeeb70635d92b3f4019138b19854e1ab617c
marina eleshaa <marinaeleshah77@gmail.com>
2024-11-11 14:48:02 +0200

    alert box

--- js/script.js
+++ js/script.js
@@ -1,29 +1,10 @@
-// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
-
-  // Messages to display in the popover
-  const messages = [
-    "First message!",
-    "Here's a second message!",
-    "And a third message just for you!"
-  ];
-  let messageIndex = 0;
-
-  // Initialize the popover
-  const popoverTriggerEl = document.getElementById('popoverButton');
-  const popover = new bootstrap.Popover(popoverTriggerEl, {
-    content: messages[messageIndex]
+var btn = document.querySelectorAll(".copied");
+btn.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    document.getElementById("alertModal").style.display = "flex";
   });
+});
 
-  // Event listener to change the message and show alert on each click
-  popoverTriggerEl.addEventListener('click', () => {
-    // Update message index and wrap around if it exceeds array length
-    messageIndex = (messageIndex + 1) % messages.length;
-
-    // Update popover content
-    popover.setContent({ content: messages[messageIndex] });
-
-    // Show an alert
-    alert("Popover message updated!");
-  });
+function closeAlert() {
+  document.getElementById("alertModal").style.display = "none";
+}
